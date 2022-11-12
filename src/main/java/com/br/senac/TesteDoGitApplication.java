00cf946dee54ca1d6a3523e76f801b70de383f36
lucianomendell <giom.01@hotmail.com>
2022-11-12 11:18:02 -0300

    primeiro commit

---
+++ src/main/java/com/br/senac/TesteDoGitApplication.java
@@ -0,0 +1,13 @@
+package com.br.senac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteDoGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteDoGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/senac/TesteDoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.senac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteDoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
